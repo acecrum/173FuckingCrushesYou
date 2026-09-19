55f86f04632b5467e29d8e8df9b59c3eddc14ee9
acecrum <axertlie@gmail.com>
2026-09-19 15:52:40 -0400

    uses grey candy damage handler now

--- 173FuckingCrushesYou/PeanutFaceSitting.cs
+++ 173FuckingCrushesYou/PeanutFaceSitting.cs
@@ -8,7 +8,7 @@ public class _173FuckingCrushesYouPlugin : Plugin
     public override string Name { get; } = "173 Fucking Crushes You";
     public override string Description { get; } = "Peanut can crush players when landing like with grey candy";
     public override string Author { get; } = "acecrum";
-    public override Version Version { get; } = new Version(1, 0, 0);
+    public override Version Version { get; } = new Version(1, 0, 1);
     public override Version RequiredApiVersion { get; } = new (LabApiProperties.CompiledVersion);
 
     private HeWhoShallPullMeOutOf173ShallBeNamedKingArthur? _eventHandler;
